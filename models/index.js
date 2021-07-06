9e2b972e84c0e9747159aacd51694e51ae8be1b0
Mark Laham <marklaham@gmail.com>
2021-07-06 10:07:52 -0400

    recent changes

--- models/index.js
+++ models/index.js
@@ -11,12 +11,12 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-// Comment.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-// Comment.belongsTo(Post, {
-//   foreignKey: 'post_id'
-// });
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
 
 module.exports = { User, Post, Comment };
